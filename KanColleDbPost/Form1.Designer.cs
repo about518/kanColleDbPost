3ac8ae71fa3e366b6b8046da0074a563fb88b440
about518 <iemon29@gmail.com>
2013-11-12 18:00:56 +0900

    Initial Version

---
+++ KanColleDbPost/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace KanColleDb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(582, 253);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.WordWrap = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(588, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "開始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(588, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "停止";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(77, 259);
+            this.textBox2.MaxLength = 27;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(308, 19);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 262);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "アクセスキー";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(607, 262);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(56, 16);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Debug";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(675, 290);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "艦これ統計データベース送信";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ KanColleDbPost/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Net;
+using System.IO;
+using System.Web;
+using Fiddler;
+
+namespace KanColleDb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Fiddler.FiddlerApplication.AfterSessionComplete += FiddlerApplication_AfterSessionComplete;
+        }
+
+        void FiddlerApplication_AfterSessionComplete(Fiddler.Session oSession)
+        {
+            string[] keys = new string[]
+            {
+		        "api_req_kousyou/createship",
+		        "api_req_kousyou/getship",
+		        "api_req_kousyou/createitem",
+		        "api_get_member/basic",
+		        "api_get_member/deck",
+		        "api_get_member/deck_port",
+                "api_get_member/ship2",
+		        "api_req_map/start",
+		        "api_req_map/next",
+		        "api_req_sortie/battle",
+		        "api_req_battle_midnight/battle",
+		        "api_req_sortie/battleresult",
+		        "api_req_practice/battle",
+		        "api_req_practice/battle_result"
+            };
+            string url = oSession.fullUrl;
+            if( url.IndexOf("/kcsapi/") <= 0 )
+            {
+                if (checkBox1.Checked)
+                {
+                    AppendText(url + "\n");
+                }
+                return;
+            }
+            foreach (string key in keys)
+            {
+                if (url.IndexOf(key) > 0)
+                {
+                    string str = "Post server from " + url + "\n";
+                    AppendText(str);
+
+                    string res = PostServer(oSession);
+                    str = "Post response : " + res + "\n";
+                    AppendText(str);
+                    return;
+                }
+            }
+            if (checkBox1.Checked)
+            {
+                AppendText(url + "\n");
+                }
+        }
+
+        private string PostServer(Fiddler.Session oSession)
+        {
+            string token = textBox2.Text;                   // TODO: ユーザー毎のトークンを設定
+            string agent = "********************";          // TODO: アプリ毎のトークンを設定
+            string url = oSession.fullUrl;
+            string requestBody = oSession.GetRequestBodyAsString();
+            string responseBody = oSession.GetResponseBodyAsString();
+
+            try
+            {
+                WebRequest req = WebRequest.Create("http://api.kancolle-db.net/1/");
+                req.Method = "POST";
+                req.ContentType = "application/x-www-form-urlencoded";
+
+                System.Text.Encoding enc = System.Text.Encoding.GetEncoding("utf-8");
+                string postdata =
+                      "token=" + HttpUtility.UrlEncode(token) + "&"
+                    + "agent=" + HttpUtility.UrlEncode(agent) + "&"
+                    + "url=" + HttpUtility.UrlEncode(url) + "&"
+                    + "requestbody=" + HttpUtility.UrlEncode(requestBody) + "&"
+                    + "responsebody=" + HttpUtility.UrlEncode(responseBody);
+                byte[] postDataBytes = System.Text.Encoding.ASCII.GetBytes(postdata);
+                req.ContentLength = postDataBytes.Length;
+
+                Stream reqStream = req.GetRequestStream();
+                reqStream.Write(postDataBytes, 0, postDataBytes.Length);
+                reqStream.Close();
+
+                WebResponse res = req.GetResponse();
+                HttpWebResponse httpRes = (HttpWebResponse)res;
+                Stream resStream = res.GetResponseStream();
+                StreamReader sr = new StreamReader(resStream, enc);
+                string response = sr.ReadToEnd();
+                sr.Close();
+                return httpRes.StatusCode + ": " + response;
+            }
+            catch (WebException ex)
+            {
+                if (ex.Status == WebExceptionStatus.ProtocolError)
+                {
+                    HttpWebResponse error = (HttpWebResponse)ex.Response;
+                    return error.ResponseUri + " " + error.StatusCode + ": " + error.StatusDescription;
+                }
+                return ex.Message;
+            }
+        }
+        
+        /// <summary>
+        /// キャプチャ開始
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Fiddler.FiddlerApplication.Startup(8877, true, false);
+            AppendText("----- Capture start\n");
+        }
+
+        /// <summary>
+        /// キャプチャ終了
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            AppendText("----- Capture stop\n");
+            Fiddler.FiddlerApplication.Shutdown();
+        }
+
+        // Windowsフォームコントロールに対して非同期な呼び出しを行うためのデリゲート
+        delegate void SetTextCallback(string text);
+
+        private void AppendText(string text)
+        {
+            // 呼び出し元のコントロールのスレッドが異なるか確認をする
+            if (this.textBox1.InvokeRequired)
+            {
+                // 同一メソッドへのコールバックを作成する
+                SetTextCallback delegateMethod = new SetTextCallback(AppendText);
+
+                // コントロールの親のInvoke()メソッドを呼び出すことで、呼び出し元の
+                // コントロールのスレッドでこのメソッドを実行する
+                this.Invoke(delegateMethod, new object[] { text });
+            }
+            else
+            {
+                // コントロールを直接呼び出す
+                this.textBox1.AppendText(text);
+                this.textBox1.SelectionStart = textBox1.Text.Length;
+                this.textBox1.ScrollToCaret();
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            // Fiddlerのシャットダウン
+            Fiddler.FiddlerApplication.Shutdown();
+        }
+    }
+}

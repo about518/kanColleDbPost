2fa1b922b6b2955dd3ac741af4dbf50a830b458c
about518 <iemon29@gmail.com>
2015-06-13 01:08:46 +0900

    FiddlerCore4.dllの更新と、不要なURLを削除

--- KanColleDbPost/Form1.cs
+++ KanColleDbPost/Form1.cs
@@ -1,38 +1,32 @@
-﻿using System;
+﻿using Fiddler;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
-using System.Diagnostics;
-using System.Net;
 using System.IO;
-using System.Web;
-using System.Threading.Tasks;
-using Fiddler;
+using System.Net;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
 
 namespace KanColleDbPost
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
 			this.ShowInTaskbar = false;
 		}
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            FiddlerApplication.AfterSessionComplete += FiddlerApplication_AfterSessionComplete;
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			FiddlerApplication.AfterSessionComplete += FiddlerApplication_AfterSessionComplete;
 			Application.ApplicationExit += Application_ApplicationExit;
-        }
+		}
 
 		void Application_ApplicationExit(object sender, EventArgs e)
 		{
-			if( isCapture )
+			if (isCapture)
 			{
 				// Fiddlerのシャットダウン
 				FiddlerApplication.Shutdown();
@@ -40,33 +34,22 @@ namespace KanColleDbPost
 			global::KanColleDbPost.Properties.Settings.Default.Save();
 		}
 
-        public enum UrlType
-        {
-            PORT,
-            SHIP2,
+		public enum UrlType
+		{
+			PORT,
+			SHIP2,
 			SHIP3,
 			KDOCK,
 			CHANGE,
-            CREATESHIP,
-            GETSHIP,
-            CREATEITEM,
-            START,
-            NEXT,
-            BATTLE,
-            BATTLE_MIDNIGHT,
-            BATTLE_SP_MIDNIGHT,
-            BATTLE_NIGHT_TO_DAY,
-            BATTLERESULT,
-            PRACTICE_BATTLE,
-            PRACTICE_BATTLERESULT,
-			COMBINED_BATTLE,
-			COMBINED_BATTLE_AIR,
-			COMBINED_BATTLE_MIDNIGHT,
+			CREATESHIP,
+			GETSHIP,
+			CREATEITEM,
+			BATTLERESULT,
 			COMBINED_BATTLE_RESULT,
-            //MASTER,
-        };
+			//MASTER,
+		};
 
-        public Dictionary<UrlType, string> urls = new Dictionary<UrlType, string>()
+		public Dictionary<UrlType, string> urls = new Dictionary<UrlType, string>()
         {
             { UrlType.PORT,                     "api_port/port"                       },
             { UrlType.SHIP2,                    "api_get_member/ship2"                },
@@ -76,152 +59,141 @@ namespace KanColleDbPost
             { UrlType.CREATESHIP,               "api_req_kousyou/createship"          },
             { UrlType.GETSHIP,                  "api_req_kousyou/getship"             },
             { UrlType.CREATEITEM,               "api_req_kousyou/createitem"          },
-            { UrlType.START,                    "api_req_map/start"                   },
-            { UrlType.NEXT,                     "api_req_map/next"                    },
-            { UrlType.BATTLE,                   "api_req_sortie/battle"               },
-            { UrlType.BATTLE_MIDNIGHT,          "api_req_battle_midnight/battle"      },
-            { UrlType.BATTLE_SP_MIDNIGHT,       "api_req_battle_midnight/sp_midnight" },
-            { UrlType.BATTLE_NIGHT_TO_DAY,      "api_req_sortie/night_to_day"         },
             { UrlType.BATTLERESULT,             "api_req_sortie/battleresult"         },
-            { UrlType.PRACTICE_BATTLE,          "api_req_practice/battle"             },
-            { UrlType.PRACTICE_BATTLERESULT,    "api_req_practice/battle_result"      },
-            { UrlType.COMBINED_BATTLE,          "api_req_combined_battle/battle"      },
-            { UrlType.COMBINED_BATTLE_AIR,      "api_req_combined_battle/airbattle"   },
-            { UrlType.COMBINED_BATTLE_MIDNIGHT, "api_req_combined_battle/midnight_battle"},
             { UrlType.COMBINED_BATTLE_RESULT,   "api_req_combined_battle/battleresult"},
-            //{ UrlType.MASTER,                   "api_start2"                          },
+			//{ UrlType.MASTER,                   "api_start2"                          },
         };
 
 		private bool isCapture = false;
 
-        void FiddlerApplication_AfterSessionComplete(Session oSession)
-        {
-            if (oSession.PathAndQuery.StartsWith("/kcsapi") &&
-                oSession.oResponse.MIMEType.Equals("text/plain"))
-            {
-                Task.Factory.StartNew(() =>
-                {
-                    string url = oSession.fullUrl;
-                    foreach (KeyValuePair<UrlType, string> kvp in urls)
-                    {
-                        if (url.IndexOf(kvp.Value) > 0)
-                        {
-                            string responseBody = oSession.GetResponseBodyAsString();
-                            responseBody.Replace("svdata=", "");
+		void FiddlerApplication_AfterSessionComplete(Session oSession)
+		{
+			if (oSession.PathAndQuery.StartsWith("/kcsapi") &&
+				oSession.oResponse.MIMEType.Equals("text/plain"))
+			{
+				Task.Factory.StartNew(() =>
+				{
+					string url = oSession.fullUrl;
+					foreach (KeyValuePair<UrlType, string> kvp in urls)
+					{
+						if (url.IndexOf(kvp.Value) > 0)
+						{
+							string responseBody = oSession.GetResponseBodyAsString();
+							responseBody.Replace("svdata=", "");
 
-                            string str = "Post server from " + url + "\n";
-                            AppendText(str);
+							string str = "Post server from " + url + "\n";
+							AppendText(str);
 
-                            string res = PostServer(oSession);
-                            str = "Post response : " + res + "\n";
-                            AppendText(str);
-                            return;
-                        }
-                    }
-                    if (checkBox1.Checked)
-                    {
-                        AppendText(url + "\n");
-                    }
-                });
-            }
-        }
+							string res = PostServer(oSession);
+							str = "Post response : " + res + "\n";
+							AppendText(str);
+							return;
+						}
+					}
+					if (checkBox1.Checked)
+					{
+						AppendText(url + "\n");
+					}
+				});
+			}
+		}
 
-        private string PostServer(Session oSession)
-        {
-            string token = textBox2.Text;                   // TODO: ユーザー毎のトークンを設定
-            string agent = "";          // TODO: アプリ毎のトークンを設定
-            string url = oSession.fullUrl;
+		private string PostServer(Session oSession)
+		{
+			string token = textBox2.Text;                   // TODO: ユーザー毎のトークンを設定
+			string agent = "";          // TODO: アプリ毎のトークンを設定
+			string url = oSession.fullUrl;
 			string requestBody = HttpUtility.HtmlDecode(oSession.GetRequestBodyAsString());
-            requestBody = Regex.Replace(requestBody, @"&api(_|%5F)token=[0-9a-f]+|api(_|%5F)token=[0-9a-f]+&?", "");	// api_tokenを送信しないように削除
-            string responseBody = oSession.GetResponseBodyAsString();
-            responseBody.Replace("svdata=", "");
+			requestBody = Regex.Replace(requestBody, @"&api(_|%5F)token=[0-9a-f]+|api(_|%5F)token=[0-9a-f]+&?", "");	// api_tokenを送信しないように削除
+			string responseBody = oSession.GetResponseBodyAsString();
+			responseBody.Replace("svdata=", "");
 
-            try
-            {
-                WebRequest req = WebRequest.Create("http://api.kancolle-db.net/2/");
-                req.Method = "POST";
-                req.ContentType = "application/x-www-form-urlencoded";
+			try
+			{
+				WebRequest req = WebRequest.Create("http://api.kancolle-db.net/2/");
+				req.Method = "POST";
+				req.ContentType = "application/x-www-form-urlencoded";
 
-                System.Text.Encoding enc = System.Text.Encoding.GetEncoding("utf-8");
-                string postdata =
-                      "token=" + HttpUtility.UrlEncode(token) + "&"
-                    + "agent=" + HttpUtility.UrlEncode(agent) + "&"
-                    + "url=" + HttpUtility.UrlEncode(url) + "&"
-                    + "requestbody=" + HttpUtility.UrlEncode(requestBody) + "&"
-                    + "responsebody=" + HttpUtility.UrlEncode(responseBody);
-                byte[] postDataBytes = System.Text.Encoding.ASCII.GetBytes(postdata);
-                req.ContentLength = postDataBytes.Length;
+				System.Text.Encoding enc = System.Text.Encoding.GetEncoding("utf-8");
+				string postdata =
+					  "token=" + HttpUtility.UrlEncode(token) + "&"
+					+ "agent=" + HttpUtility.UrlEncode(agent) + "&"
+					+ "url=" + HttpUtility.UrlEncode(url) + "&"
+					+ "requestbody=" + HttpUtility.UrlEncode(requestBody) + "&"
+					+ "responsebody=" + HttpUtility.UrlEncode(responseBody);
+				byte[] postDataBytes = System.Text.Encoding.ASCII.GetBytes(postdata);
+				req.ContentLength = postDataBytes.Length;
 
-                Stream reqStream = req.GetRequestStream();
-                reqStream.Write(postDataBytes, 0, postDataBytes.Length);
-                reqStream.Close();
+				Stream reqStream = req.GetRequestStream();
+				reqStream.Write(postDataBytes, 0, postDataBytes.Length);
+				reqStream.Close();
 
-                WebResponse res = req.GetResponse();
-                HttpWebResponse httpRes = (HttpWebResponse)res;
-                Stream resStream = res.GetResponseStream();
-                StreamReader sr = new StreamReader(resStream, enc);
-                string response = sr.ReadToEnd();
-                sr.Close();
-                return oSession.responseCode + ": " + response;
-            }
-            catch (WebException ex)
-            {
-                if (ex.Status == WebExceptionStatus.ProtocolError)
-                {
-                    HttpWebResponse error = (HttpWebResponse)ex.Response;
-                    return error.ResponseUri + " " + oSession.responseCode + ": " + error.StatusDescription;
-                }
-                return ex.Message;
-            }
-        }
-        
-        /// <summary>
-        /// キャプチャ開始
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if( !isCapture )
-            {
-                FiddlerApplication.Startup(8877, true, false);
+				WebResponse res = req.GetResponse();
+				HttpWebResponse httpRes = (HttpWebResponse)res;
+				Stream resStream = res.GetResponseStream();
+				StreamReader sr = new StreamReader(resStream, enc);
+				string response = sr.ReadToEnd();
+				sr.Close();
+				return oSession.responseCode + ": " + response;
+			}
+			catch (WebException ex)
+			{
+				if (ex.Status == WebExceptionStatus.ProtocolError)
+				{
+					HttpWebResponse error = (HttpWebResponse)ex.Response;
+					return error.ResponseUri + " " + oSession.responseCode + ": " + error.StatusDescription;
+				}
+				return ex.Message;
+			}
+		}
+
+		/// <summary>
+		/// キャプチャ開始
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (!isCapture)
+			{
+				FiddlerApplication.Startup(8877, FiddlerCoreStartupFlags.ChainToUpstreamGateway | FiddlerCoreStartupFlags.RegisterAsSystemProxy | FiddlerCoreStartupFlags.OptimizeThreadPool | FiddlerCoreStartupFlags.MonitorAllConnections);
 				isCapture = true;
-                AppendText("----- Capture start\n");
-                button1.Text = "停止";
-            }
-            else
-            {
-                AppendText("----- Capture stop\n");
-                FiddlerApplication.Shutdown();
+				AppendText("----- Capture start\n");
+				button1.Text = "停止";
+			}
+			else
+			{
+				AppendText("----- Capture stop\n");
+				FiddlerApplication.Shutdown();
 				isCapture = false;
-                button1.Text = "開始";
-            }
-        }
+				button1.Text = "開始";
+			}
+		}
 
 
-        // Windowsフォームコントロールに対して非同期な呼び出しを行うためのデリゲート
-        delegate void SetTextCallback(string text);
+		// Windowsフォームコントロールに対して非同期な呼び出しを行うためのデリゲート
+		delegate void SetTextCallback(string text);
 
-        private void AppendText(string text)
-        {
-            // 呼び出し元のコントロールのスレッドが異なるか確認をする
-            if (this.textBox1.InvokeRequired)
-            {
-                // 同一メソッドへのコールバックを作成する
-                SetTextCallback delegateMethod = new SetTextCallback(AppendText);
+		private void AppendText(string text)
+		{
+			// 呼び出し元のコントロールのスレッドが異なるか確認をする
+			if (this.textBox1.InvokeRequired)
+			{
+				// 同一メソッドへのコールバックを作成する
+				SetTextCallback delegateMethod = new SetTextCallback(AppendText);
 
-                // コントロールの親のInvoke()メソッドを呼び出すことで、呼び出し元の
-                // コントロールのスレッドでこのメソッドを実行する
-                this.Invoke(delegateMethod, new object[] { text });
-            }
-            else
-            {
-                // コントロールを直接呼び出す
-                this.textBox1.AppendText(text);
-                this.textBox1.SelectionStart = textBox1.Text.Length;
-                this.textBox1.ScrollToCaret();
-            }
-        }
+				// コントロールの親のInvoke()メソッドを呼び出すことで、呼び出し元の
+				// コントロールのスレッドでこのメソッドを実行する
+				this.Invoke(delegateMethod, new object[] { text });
+			}
+			else
+			{
+				// コントロールを直接呼び出す
+				this.textBox1.AppendText(text);
+				this.textBox1.SelectionStart = textBox1.Text.Length;
+				this.textBox1.ScrollToCaret();
+			}
+		}
 
 		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
 		{
@@ -237,7 +209,7 @@ namespace KanColleDbPost
 				this.Hide();
 				// トレイリストのアイコンを表示する  
 				notifyIcon1.Visible = true;
-			}   
+			}
 		}
 
 		private void notifyIcon1_DoubleClick(object sender, EventArgs e)
@@ -264,5 +236,5 @@ namespace KanColleDbPost
 		}
 
 
-    }
+	}
 }
